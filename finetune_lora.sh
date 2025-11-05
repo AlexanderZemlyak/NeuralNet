69951260cb85a37bde863c16bc82409c1b67021a
AlexanderZemlyak <zemlyaksanya@mail.ru>
2025-11-05 12:50:48 +0300

    Add lora finetuning bash script

---
+++ finetune_lora.sh
@@ -0,0 +1,18 @@
+#!bash
+python finetune_deepseekcoder2.py \
+    --model_name_or_path deepseek-ai/deepseek-coder-1.3b-instruct \
+    --data_path ./train_data.json \
+    --output_dir /LoRa \
+    --num_train_epochs 5 \
+    --per_device_train_batch_size 16 \
+    --gradient_accumulation_steps 4 \
+    --save_strategy "steps" \
+    --save_steps 25 \
+    --learning_rate 2e-4 \
+    --warmup_steps 10 \
+    --logging_steps 10 \
+    --lr_scheduler_type "cosine" \
+    --gradient_checkpointing True \
+    --report_to "tensorboard" \
+    --bf16 True \
+    --model_max_length 1024
