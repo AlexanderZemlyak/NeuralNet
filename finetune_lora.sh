f06f799d5408c80d24868c25652ae021306b4358
AlexanderZemlyak <zemlyaksanya@mail.ru>
2025-11-20 20:47:23 +0300

    Update train_data

--- finetune_lora.sh
+++ finetune_lora.sh
@@ -3,13 +3,13 @@ python finetune_deepseekcoder2.py \
     --model_name_or_path deepseek-ai/deepseek-coder-1.3b-instruct \
     --data_path ./train_data.json \
     --output_dir /LoRa \
-    --num_train_epochs 5 \
+    --num_train_epochs 10 \
     --per_device_train_batch_size 16 \
     --gradient_accumulation_steps 4 \
     --save_strategy "steps" \
     --save_steps 25 \
     --learning_rate 2e-4 \
-    --warmup_steps 10 \
+    --warmup_steps 30 \
     --logging_steps 10 \
     --lr_scheduler_type "cosine" \
     --gradient_checkpointing True \
