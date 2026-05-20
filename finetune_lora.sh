a2c11fc41f5731cf659db26ab75a941c011eeca3
Александр Земляк <zemlyaksanya@mail.ru>
2026-05-20 16:45:03 +0300

    fixes

--- finetune_lora.sh
+++ finetune_lora.sh
@@ -2,12 +2,12 @@
 deepspeed --num_gpus 2 finetune_deepseekcoder2_normal+enriched.py \
     --model_name_or_path deepseek-ai/deepseek-coder-1.3b-instruct \
     --data_path ./train_data_normal+enriched.json \
-    --output_dir ./LoRa \
+    --output_dir .LoRa \
     --num_train_epochs 5 \
     --per_device_train_batch_size 16 \
     --gradient_accumulation_steps 1 \
     --save_strategy "steps" \
-    --save_steps 1 \
+    --save_steps 50 \
     --learning_rate 2e-4 \
     --warmup_steps 100 \
     --logging_steps 10 \
